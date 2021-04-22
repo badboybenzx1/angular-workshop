9d0be8a4f01691ac5e06a719f145df3ba04b7af6
naphon phattamabun <naphon.p@kbtg.tech>
2021-04-22 12:20:09 +0700

    feat (module): 🎨 add layout, grid, divider module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,33 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { WorkshopListComponent } from './components/workshop-list/workshop-list.component';
+import { WorkshopFormBindingComponent } from './components/workshop-form-binding/workshop-form-binding.component';
+import { LoginComponent } from './components/login/login.component';
+
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkshopListComponent,
+    WorkshopFormBindingComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    NzGridModule,
+    NzCardModule,
+    NzDividerModule
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
