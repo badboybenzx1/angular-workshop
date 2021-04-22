6db5a30a4706340b4d41915b609c5c454ec5d15d
naphon phattamabun <naphon.p@kbtg.tech>
2021-04-22 16:30:57 +0700

    feat (main route): ✨ add main and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { QuizComponent } from './components';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'workshop', component: AppComponent },
+  { path: 'quiz', component: QuizComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 import {
+  MainComponent,
   WorkshopListComponent,
   WorkshopFormBindingComponent,
   LoginComponent,
   WorkshopPipeComponent,
   WorkshopRatingComponent,
   RatingComponent,
+  QuizComponent,
 } from './components';
 
 import { ReplaceWithDashPipe, UppercaseWithDotPipe } from './pipes';
@@ -40,6 +43,8 @@ registerLocaleData(en);
     UppercaseWithDotPipe,
     WorkshopRatingComponent,
     RatingComponent,
+    MainComponent,
+    QuizComponent,
   ],
   imports: [
     BrowserModule,
@@ -48,11 +53,12 @@ registerLocaleData(en);
     HttpClientModule,
     BrowserAnimationsModule,
     NzLayoutModule,
+    NzMenuModule,
     NzGridModule,
     NzCardModule,
     NzDividerModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
-  bootstrap: [AppComponent],
+  bootstrap: [MainComponent],
 })
 export class AppModule {}
