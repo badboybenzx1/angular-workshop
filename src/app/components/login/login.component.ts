39df4b5fdc2e57acd78cc59fad555b6d4a03f6d3
naphon phattamabun <naphon.p@kbtg.tech>
2021-04-22 14:11:52 +0700

    feat (workshop 4): ✨ user class and display inside table

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  users: User[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.users = [
+      new User(1, 'johnny', 'depp', 'johnny.depp@gmail.com'),
+      new User(2, 'leonardo', 'dicaprio', 'leonardo.dicaprio@gmail.com'),
+      new User(3, 'tom', 'hank', 'tom.hank@gmail.com'),
+    ];
   }
-
 }
